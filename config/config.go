0e9f7e40c1c12f0806bb5c02f255b18b4d23116b
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-11-08 23:55:05 +0700

    fix: Added server url to config

--- config/config.go
+++ config/config.go
@@ -9,6 +9,7 @@ type Config struct {
 	ServerAddress string `mapstructure:"server_address"`
 	Debug         bool   `mapstructure:"debug"`
 	GinMode       string `mapstructure:"gin_mode"`
+	ServerUrl     string `mapstructure:"server_url"`
 	DB_Driver     string `mapstructure:"db_driver"`
 	DB_Hostname   string `mapstructure:"db_host"`
 	DB_Port       int    `mapstructure:"db_port"`
@@ -16,6 +17,8 @@ type Config struct {
 	DB_Password   string `mapstructure:"db_password"`
 	DB_Name       string `mapstructure:"db_name"`
 }
+
+var ServerUrl string
 
 func LoadConfig(path string) (config Config, err error) {
 	viper.SetConfigName("development")
@@ -27,6 +30,8 @@ func LoadConfig(path string) (config Config, err error) {
 	if err != nil {
 		return
 	}
+
+	ServerUrl = viper.GetString("server_url")
 
 	err = viper.Unmarshal(&config)
 	return
