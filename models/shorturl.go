3f5497a787215f63bb4a452aeb6499131fd733a8
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-11-09 00:18:20 +0700

    fix: id to lowercase and created_at from json to db

--- models/shorturl.go
+++ models/shorturl.go
@@ -3,9 +3,9 @@ package models
 import "time"
 
 type ShortUrl struct {
-	Id        int       `db:"ID"`
+	Id        int       `db:"id"`
 	FullUrl   string    `db:"full_url"`
 	Url       string    `db:"url"`
 	Click     int       `db:"click"`
-	CreatedAt time.Time `json:"created_at"`
+	CreatedAt time.Time `db:"created_at"`
 }
